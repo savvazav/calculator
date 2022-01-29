b537c73de5fc0c17b0352732608ef6edc0e953e6
savvazav <savvazav2008@gmail.com>
2022-01-29 17:27:46 +0300

    add do-while

--- main.cpp
+++ main.cpp
@@ -3,7 +3,36 @@
 
 int main () {
 
-    std::cout << "Calculator opened!" << std::endl;
+    std::cout << "Welcome to the calculator!" << std::endl;
+
+
+//int это целое число
+//double это рациональное (то есть любое, дробное!!!)
+
+    int status = 0;
+
+    do {
+        double a; 
+        std::cout << "Please, enter a: ";
+        std::cin >> a;
+
+        double b;
+        std::cout << "Please, enter b: ";
+        std::cin >> b;
+
+        std::cout << "a + b = " << a + b << std::endl;
+        std::cout << "a - b = " << a - b << std::endl;
+        std::cout << "a * b = " << a * b << std::endl;
+        std::cout << "a : b = " << a / b << std::endl << std::endl;
+
+        std::cout << "Do you want to leave? Yes(1), No(0):";
+        std::cin >> status;
+
+    } while (status == 0);
+    
+
+    std::cout << "Goodbye!" << std::endl;
+    
     return 0;
 
 };
